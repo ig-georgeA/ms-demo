44b56106fcb986801b50c9c1b9b1e3a57dfe92fc
George Abraham <41584300+ig-georgeA@users.noreply.github.com>
2024-07-23 12:45:29 -0400

    Team Collaboration updates

--- src/app/master-view/backlog/backlog.component.ts
+++ src/app/master-view/backlog/backlog.component.ts
@@ -3,9 +3,9 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IGX_GRID_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IGX_LIST_DIRECTIVES, IGX_TABS_DIRECTIVES, IgxAvatarComponent, IgxButtonDirective, IgxIconButtonDirective, IgxIconComponent, IgxOverlayOutletDirective, IgxRippleDirective, IgxToggleActionDirective, IgxToggleDirective } from '@infragistics/igniteui-angular';
 import { Subject, takeUntil } from 'rxjs';
 import { CustomersType } from '../../models/northwind/customers-type';
-import { EmployeesType } from '../../models/northwind/employees-type';
-import { OrdersType } from '../../models/northwind/orders-type';
 import { SuppliersType } from '../../models/northwind/suppliers-type';
+import { OrdersType } from '../../models/northwind/orders-type';
+import { EmployeesType } from '../../models/northwind/employees-type';
 import { NorthwindService } from '../../services/northwind.service';
 
 @Component({
